d37d5377c8f6aa9cd1ec3b128b24b68659c5fe88
qwerty <sher5624067@gmail.com>
2025-06-12 14:30:53 +0500

    reset migrations

--- kb/settings.py
+++ kb/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    #"social_django.middleware.SocialAuthExceptionMiddleware",
+    "social_django.middleware.SocialAuthExceptionMiddleware",
 ]
 
 
